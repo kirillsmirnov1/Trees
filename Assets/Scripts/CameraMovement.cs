3c68b87f8e634f8ffbf12c8b123695a5c747dda6
Kirill Smirnov <thegaldren@gmail.com>
2020-05-26 16:12:20 +0300

    move camera using mouse and touchscreen

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,19 +8,85 @@ public class CameraMovement : MonoBehaviour
     public float turnSpeed;
     public Transform tree;
 
+    // Camera position
     private Vector3 offset;
+    private bool movingCamera;
 
+    private Vector3 lastMousePos;
+    private Vector3 currentMousePos;
+    private Vector3 swipeInput;
     private void Start()
     {
-        offset = new Vector3(tree.position.x, tree.position.y, tree.position.z - 5);    
+        offset = new Vector3(tree.position.x, tree.position.y, tree.position.z - 5);
     }
 
     void Update()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        offset = Quaternion.AngleAxis(-1 * horizontalInput * turnSpeed, Vector3.up) * offset;
-        
+        HandleSwipeInput();
+
+        float horizontalInput = Input.GetAxis("Horizontal") + swipeInput.x;
+
+        offset = Quaternion.AngleAxis(horizontalInput * turnSpeed, Vector3.up) * offset;
+
         transform.position = tree.position + offset;
         transform.LookAt(tree.position);
+    }
+
+    private void HandleSwipeInput()
+    {
+        HandleMouseSwipeInput();
+        HandleScreenTouchScreenSwipeInput();
+    }
+
+    private void HandleScreenTouchScreenSwipeInput()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            if (touch.phase == TouchPhase.Began)
+            {
+                movingCamera = true;
+            }
+            else if (touch.phase == TouchPhase.Ended || touch.phase == TouchPhase.Canceled)
+            {
+                movingCamera = false;
+                swipeInput = Vector3.zero;
+            }
+            else 
+            {
+                // If touch is neither began nor finished, it's in progress
+                swipeInput = touch.deltaPosition;
+            }
+        }
+    }
+
+    private void HandleMouseSwipeInput()
+    {
+        // Swipe began
+        if (Input.GetButtonDown("Fire1"))
+        {
+            movingCamera = true;
+            lastMousePos = Input.mousePosition;
+        }
+
+        // Swipe finished
+        if (Input.GetButtonUp("Fire1"))
+        {
+            movingCamera = false;
+            swipeInput = Vector3.zero;
+        }
+
+        // Swipe in progress
+        if (Input.GetButton("Fire1"))
+        {
+            currentMousePos = Input.mousePosition;
+        }
+
+        if (movingCamera)
+        {
+            swipeInput = currentMousePos - lastMousePos;
+            lastMousePos = currentMousePos;
+        }
     }
 }
