f3522300d93ceac4547b1886836b866ffa444fb6
Kirill Smirnov <thegaldren@gmail.com>
2020-05-27 14:35:41 +0300

    generate new branch by pressing space

---
+++ Assets/Scripts/GenerateNewBranch.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateNewBranch : MonoBehaviour
+{
+    public GameObject branch;
+
+    private int subBranches = 0;
+    private int subBranchesLimit = 1;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space) && subBranches < subBranchesLimit)
+        {
+            GenerateBranch();
+        }
+    }
+
+    private void GenerateBranch()
+    {
+        subBranches++;
+        Instantiate(branch, transform.position + Vector3.up, transform.rotation);
+    }
+}
