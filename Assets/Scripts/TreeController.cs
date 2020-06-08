efbd1a78d1be157a1b2423761bb4ad3e928dba41
Kirill Smirnov <thegaldren@gmail.com>
2020-06-08 16:02:17 +0300

    use B instead of Space to generate branches

--- Assets/Scripts/TreeController.cs
+++ Assets/Scripts/TreeController.cs
@@ -40,7 +40,7 @@ public class TreeController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
+        if(Input.GetKeyDown(KeyCode.B))
         {
             GenerateNewBranches();
         }
