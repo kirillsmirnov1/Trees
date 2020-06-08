6a75fa15c396afb1b9857f1bc26a405c0b3f705b
Kirill Smirnov <thegaldren@gmail.com>
2020-06-08 17:19:01 +0300

    use Esc to lock mouse out of the game

--- Assets/Scripts/PlayerInputHandler.cs
+++ Assets/Scripts/PlayerInputHandler.cs
@@ -7,6 +7,8 @@ public class PlayerInputHandler : MonoBehaviour
     public float lookSensitivity = 1f;
     public float webglLookSensitivityMultiplier = 0.25f;
 
+    private bool cursorUnlocked = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,12 +16,34 @@ public class PlayerInputHandler : MonoBehaviour
         Cursor.visible = false;
     }
 
+    void Update()
+    {
+        if (!cursorUnlocked && Input.GetKeyDown(KeyCode.Escape))
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+            cursorUnlocked = true;
+        }
+
+        if(cursorUnlocked && Input.GetMouseButtonDown(0))
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+            cursorUnlocked = false;
+        }
+    }
+
     public Vector3 GetMoveInput()
     {
-        Vector3 move = new Vector3(Input.GetAxisRaw(GameConstants.k_AxisNameHorizontal), 0f, Input.GetAxisRaw(GameConstants.k_AxisNameVertical));
-        move = Vector3.ClampMagnitude(move, 1);
+        if (CanProcessInput())
+        {
+            Vector3 move = new Vector3(Input.GetAxisRaw(GameConstants.k_AxisNameHorizontal), 0f, Input.GetAxisRaw(GameConstants.k_AxisNameVertical));
+            move = Vector3.ClampMagnitude(move, 1);
 
-        return move;
+            return move;
+        }
+
+        return Vector3.zero;
     }
 
     public float GetLookInputsHorizontal()
@@ -34,23 +58,33 @@ public class PlayerInputHandler : MonoBehaviour
 
     float GetLookAxis(string mouseInputName)
     {
-        float i = Input.GetAxisRaw(mouseInputName);
-
-        if(mouseInputName == GameConstants.k_MouseAxisNameVertical)
+        if (CanProcessInput())
         {
-            i *= -1;
-        }
+            float i = Input.GetAxisRaw(mouseInputName);
 
-        // apply sensitivity multiplier
-        i *= lookSensitivity;
+            if (mouseInputName == GameConstants.k_MouseAxisNameVertical)
+            {
+                i *= -1;
+            }
 
-        // reduce mouse input amount to be equivalent to stick movement
-        i *= 0.01f;
+            // apply sensitivity multiplier
+            i *= lookSensitivity;
+
+            // reduce mouse input amount to be equivalent to stick movement
+            i *= 0.01f;
 #if UNITY_WEBGL
-        // Mouse tends to be even more sensitive in WebGL due to mouse acceleration, so reduce it even more
-        i *= webglLookSensitivityMultiplier;
+            // Mouse tends to be even more sensitive in WebGL due to mouse acceleration, so reduce it even more
+            i *= webglLookSensitivityMultiplier;
 #endif
 
-        return i;
+            return i;
+        }
+
+        return 0;
+    }
+
+    public bool CanProcessInput()
+    {
+        return Cursor.lockState == CursorLockMode.Locked;
     }
 }
