1c6f19d1ec50c366fcae3400a1ab9f8ec38636d0
Kirill Smirnov <thegaldren@gmail.com>
2020-06-10 11:38:36 +0300

    make branches call tree.GenerateBranches() on click

---
+++ Assets/BranchInputHandler.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BranchInputHandler : MonoBehaviour
+{
+    private TreeController _treeController;
+    private TreeController TreeController 
+    { 
+        get 
+        { 
+            if(_treeController == null)
+            {
+                _treeController = GameObject.Find("Tree").GetComponent<TreeController>();
+            }
+            return _treeController;
+        } 
+    }
+
+    private void OnMouseDown()
+    {
+        TreeController.GenerateNewBranches();
+    }
+}
