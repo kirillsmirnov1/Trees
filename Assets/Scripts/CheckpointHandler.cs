3a6985c37c7a4c6a7ec5a05d2c0679a163135e53
Kirill Smirnov <thegaldren@gmail.com>
2020-06-10 16:28:29 +0300

    Player checks checkpoints when in fog

---
+++ Assets/Scripts/CheckpointHandler.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckpointHandler : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        GameObject.Find("Player").GetComponent<PlayerCheckpointHandler>().AddCheckPoint(gameObject);
+        Debug.Log("Checkpoint activated");
+    }
+}
---
+++ Assets/Scripts/PlayerCheckpointHandler.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class PlayerCheckpointHandler : MonoBehaviour
+{
+    private List<GameObject> checkPoints = new List<GameObject>();
+
+    public void AddCheckPoint(GameObject checkPoint)
+    {
+        checkPoints.Add(checkPoint);
+    }
+
+    private void OnParticleCollision(GameObject other)
+    {
+        if (other.CompareTag("Fog"))
+        {
+            HandleCheckPoints();
+        }
+    }
+
+    private void HandleCheckPoints()
+    {
+        GameObject closestCp = null;
+        float minDistance = float.MaxValue;
+
+        foreach(GameObject checkPoint in checkPoints)
+        {
+            Debug.DrawLine(checkPoint.transform.position, transform.position, Color.blue, 0.01f);
+            float d = Vector3.Distance(checkPoint.transform.position, transform.position);
+            if (d < minDistance)
+            {
+                closestCp = checkPoint;
+                minDistance = d;
+            }
+        }
+
+        Debug.DrawLine(transform.position, closestCp.transform.position, Color.green, 0.02f);
+    }
+}
