b7a19e5b12605633dd80e13abed6ddee63898d95
Kirill Smirnov <thegaldren@gmail.com>
2020-06-25 12:20:02 +0300

    fogDestroyers.Awake() adds itself to FogController

---
+++ Assets/FogDestroyer.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class FogDestroyer : MonoBehaviour
+{
+    private void Awake()
+    {
+        foreach(var fog in FindObjectsOfType<FogController>())
+        {
+            fog.AddDestroyer(GetComponent<Collider>());
+        }
+    }
+}
--- Assets/Scripts/FogController.cs
+++ Assets/Scripts/FogController.cs
@@ -2,27 +2,8 @@
 
 public class FogController : MonoBehaviour
 {
-    private const string FogDestroyerTag = "FogDestroyer";
     private int numberOfColliders = 0;
-    private ParticleSystem.TriggerModule triggerModule;
 
-    void Start()
-    {
-        triggerModule = GetComponent<ParticleSystem>().trigger;
-        FindFogDestroyers();
-    }
-
-    private void FindFogDestroyers()
-    {
-        var triggers = GameObject.FindGameObjectsWithTag(FogDestroyerTag);
-        numberOfColliders = triggers.Length;
-
-        for (int i = 0; i < numberOfColliders; ++i)
-        {
-            triggerModule.SetCollider(i, triggers[i].GetComponent<Collider>());
-        }
-    }
-
-    public void AddDestroyer(Collider collider) => 
-        triggerModule.SetCollider(numberOfColliders++, collider);
+    public void AddDestroyer(Collider collider) =>
+        GetComponent<ParticleSystem>().trigger.SetCollider(numberOfColliders++, collider);
 }
