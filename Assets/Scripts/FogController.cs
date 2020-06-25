ecc12d15284ea1510d3bdbf1aa2ee2b554b5b160
Kirill Smirnov <thegaldren@gmail.com>
2020-06-25 12:10:25 +0300

    update fog script — AddDestroyer()

---
+++ Assets/Scripts/FogController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class FogController : MonoBehaviour
+{
+    private const string FogDestroyerTag = "FogDestroyer";
+    private int numberOfColliders = 0;
+    private ParticleSystem.TriggerModule triggerModule;
+
+    void Start()
+    {
+        triggerModule = GetComponent<ParticleSystem>().trigger;
+        FindFogDestroyers();
+    }
+
+    private void FindFogDestroyers()
+    {
+        var triggers = GameObject.FindGameObjectsWithTag(FogDestroyerTag);
+        numberOfColliders = triggers.Length;
+
+        for (int i = 0; i < numberOfColliders; ++i)
+        {
+            triggerModule.SetCollider(i, triggers[i].GetComponent<Collider>());
+        }
+    }
+
+    public void AddDestroyer(Collider collider) => 
+        triggerModule.SetCollider(numberOfColliders++, collider);
+}
