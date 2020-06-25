d516975cbc2106906d72b30123278d5599265677
Kirill Smirnov <thegaldren@gmail.com>
2020-06-25 16:15:33 +0300

    fix fog triggers

--- Assets/Scripts/Fog.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-public class Fog : MonoBehaviour
-{
-    private int numberOfColliders = 0;
-
-    public void AddDestroyer(Collider collider) =>
-        GetComponent<ParticleSystem>().trigger.SetCollider(numberOfColliders++, collider);
-}
