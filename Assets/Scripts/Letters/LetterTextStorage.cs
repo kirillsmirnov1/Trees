39c604900eae76c488d9aa9fd07e70a5b143de2a
Kirill Smirnov <thegaldren@gmail.com>
2020-06-26 12:05:59 +0300

    move letter scripts

---
+++ Assets/Scripts/Letters/LetterTextStorage.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LetterTextStorage : MonoBehaviour
+{
+    public static Dictionary<string, string> Text = new Dictionary<string, string> 
+    {
+        ["01"] = "Oh well, that is first letter indeed",
+        ["02"] = "You found the second one, that a good seeker you are!",
+        ["03"] = "TODO",
+        ["04"] = "TODO",
+        ["05"] = "TODO",
+        ["barn"] = "TODO",
+        ["cook"] = "TODO",
+        ["commander"] = "TODO",
+        ["soldier"] = "TODO",
+        ["worker"] = "TODO",
+        ["06"] = "TODO",
+        ["07"] = "TODO",
+        ["08"] = "TODO",
+        ["09"] = "TODO",
+        ["final"] = "TODO"
+    };
+}
