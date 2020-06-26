a7337bf73f097f64b29baf05cb4566e6c92bdc61
Kirill Smirnov <thegaldren@gmail.com>
2020-06-26 10:41:07 +0300

    Update LetterTextStorage.cs

--- Assets/Scripts/LetterTextStorage.cs
+++ Assets/Scripts/LetterTextStorage.cs
@@ -6,7 +6,7 @@ public class LetterTextStorage : MonoBehaviour
 {
     public static Dictionary<string, string> Text = new Dictionary<string, string> 
     {
-        ["first"] = "Oh well, that is first letter indeed",
-        ["second"] = "You found the second one, that a good seeker you are!"
+        ["01"] = "Oh well, that is first letter indeed",
+        ["02"] = "You found the second one, that a good seeker you are!"
     };
 }
