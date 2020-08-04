92ea5eb930bfd622d646fefd577acffbe81a555e
Kirill Smirnov <thegaldren@gmail.com>
2020-08-04 17:39:01 +0300

    Add TreeEditor.cs

---
+++ Assets/__Project/Scripts/Editor/TreeEditor.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(TreeController))]
+public class TreeEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        TreeController tree = (TreeController)target;
+
+        if(GUILayout.Button("Grow some branches"))
+        {
+            tree.GenerateNewBranches();
+        }
+
+        if(GUILayout.Button("Grow tree"))
+        {
+            tree.GrowFullTree();
+        }
+
+        if(GUILayout.Button("Reset tree"))
+        {
+            tree.ResetTree();
+        }
+    }
+}
