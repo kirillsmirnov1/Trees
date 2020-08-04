f6698ab2de4c203507a3206466fde87057a0a8ff
Kirill Smirnov <thegaldren@gmail.com>
2020-08-04 17:26:43 +0300

    Make SceneSettings a singlton

--- Assets/__Project/Scripts/SceneSettings.cs
+++ Assets/__Project/Scripts/SceneSettings.cs
@@ -1,10 +1,16 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class SceneSettings : MonoBehaviour
 {
+    public static SceneSettings Instance { get; private set; }
+
     public SceneType currentScene;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
     public enum SceneType
     {
         FloatingTree,
